25e9b48e2dd42ffb73655396c587818b792b0c8b
Sadaf Jalilzadeh <utashell@Sadafs-MacBook-Air.local>
2026-02-03 13:36:54 +0330

    Setup DRF API for Tasks

--- todo/admin.py
+++ todo/admin.py
@@ -5,7 +5,7 @@ from . import models
 
 @admin.register(models.Todo)
 class Todoadmin(admin.ModelAdmin):
-    list_display = ('title', 'status_colored', 'is_done', 'priority', 'created_at')
+    list_display = ('title', 'status_colored', 'is_done', 'priority', 'created_at', 'updated_at', 'due_date')
     list_filter = ('is_done', 'priority', 'created_at')
     search_fields = ('title', 'description')
     list_editable = ('is_done',)
